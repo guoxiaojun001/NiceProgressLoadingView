0f595df32c9cc09a2fd2be882ff99be354872564
kaku <guoxiaojun8804@126.com>
2016-06-23 15:32:34 +0800

    漂亮的加载动画

---
+++ README.md
@@ -0,0 +1,1 @@
+本文参照：http://www.jianshu.com/p/52d0e9f6e27b
---
+++ app/src/main/java/freeman/rx/gxj/com/niceprogressloadingview/MainActivity.java
@@ -0,0 +1,13 @@
+package freeman.rx.gxj.com.niceprogressloadingview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
